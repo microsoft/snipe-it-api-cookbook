b98264b072633096c8422272651cbabd92812ea6
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-10-11 19:40:22 -0700

    Add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'snipeit_api'
+maintainer 'Microsoft'
+maintainer_email 'chef@microsoft.com'
+license 'MIT'
+description 'Installs/Configures snipeit_api'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+chef_version '>= 14.0'
+source_url 'https://github.com/Microsoft/snipe-it-api-cookbook'
+issues_url 'https://github.com/Microsoft/snipe-it-api-cookbook/issues'
+
+supports 'mac_os_x'
+
+depends 'chef-sugar'
+depends 'chef-vault'
