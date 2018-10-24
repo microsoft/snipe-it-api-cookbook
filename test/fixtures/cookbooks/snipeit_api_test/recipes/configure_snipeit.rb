4b3960ac73d505a632b6b3c266614f5a448243e2
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-10-24 14:27:50 -0700

    Rely on the API token attribute being set by the custom resources

--- test/fixtures/cookbooks/snipeit_api_test/recipes/configure_snipeit.rb
+++ test/fixtures/cookbooks/snipeit_api_test/recipes/configure_snipeit.rb
@@ -1,5 +1,3 @@
-api_token = chef_vault_item('snipeit', 'api')['key']
-
 categories = {
   'macOS - Desktop' => 'asset',
   'macOS - Portable' => 'asset',
@@ -7,13 +5,10 @@ categories = {
 
 manufacturer 'Apple' do
   website 'https://www.apple.com'
-  token api_token
-  action :create
 end
 
 categories.each do |name, type|
   category name do
-    token api_token
     category_type type
   end
 end
@@ -22,14 +17,12 @@ model 'Mac Pro (Early 2009)' do
   manufacturer 'Apple'
   category 'macOS - Desktop'
   model_number 'MacPro4,1'
-  token api_token
 end
 
 asset '1234567' do
   serial_number 'HALAEK123123'
   status 'Pending'
   model 'Mac Pro (Early 2009)'
-  token api_token
 end
 
 location 'Building 1' do
