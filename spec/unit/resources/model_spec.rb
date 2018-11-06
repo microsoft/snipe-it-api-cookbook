a6714f19c964d10f4c29e81f1f8b620f164ecd2a
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-11-06 12:19:34 -0800

    Add failed tests for model and asset

--- spec/unit/resources/model_spec.rb
+++ spec/unit/resources/model_spec.rb
@@ -1,41 +1,32 @@
 require 'spec_helper'
 
-shared_examples 'model' do
-  step_into :model
-  recipe do
-    api_token = node['snipeit']['api']['token']
-    url = node['snipeit']['api']['instance']
-
-    model 'Mac Pro (Early 2009)' do
-      manufacturer 'Apple'
-      category 'macOS - Desktop'
-      model_number 'MacPro4,1'
-      token api_token
-      url url
-    end
-
-    model 'HAL 9000' do
-      manufacturer 'University of Illinois'
-      category 'Windows - Desktop'
-      model_number 'HAL9000'
-      token api_token
-      url url
-    end
-  end
-end
-
 describe 'lab_core::model' do
-  url = 'http://fakeymcfakerton.corp.mycompany.com/api/v1/models'
-  include_examples 'model'
-
+  step_into :model
   context 'when the model does exist' do
-    it {
-      is_expected.to_not post_http_request('create model[Mac Pro (Early 2009)]')
-        .with(url: url, headers: headers)
-    }
+    recipe do
+      model 'Mac Pro (Early 2009)' do
+        manufacturer 'Apple'
+        category 'macOS - Desktop'
+        model_number 'MacPro4,1'
+        token node['snipeit']['api']['token']
+        url node['snipeit']['api']['instance']
+      end
+    end
+
+    it { is_expected.to_not post_http_request('create model[MacPro4,1]') }
   end
 
   context 'when the model does not exist' do
+    recipe do
+      model 'HAL 9000' do
+        manufacturer 'University of Illinois'
+        category 'Windows - Desktop'
+        model_number 'HAL9000'
+        token node['snipeit']['api']['token']
+        url node['snipeit']['api']['instance']
+      end
+    end
+
     message = {
       name: 'HAL 9000',
       model_number: 'HAL9000',
@@ -45,7 +36,27 @@ describe 'lab_core::model' do
 
     it {
       is_expected.to post_http_request('create model[HAL 9000]')
-        .with(url: url, message: message.to_json, headers: headers)
+        .with(
+          url: 'http://fakeymcfakerton.corp.mycompany.com/api/v1/models',
+          message: message.to_json,
+          headers: headers
+        )
     }
+  end
+
+  context 'when the manufacturer does not exist' do
+    recipe do
+      model 'Altair 8800' do
+        manufacturer 'MITS'
+        category 'Windows - Desktop'
+        model_number 'Altair8800'
+        token node['snipeit']['api']['token']
+        url node['snipeit']['api']['instance']
+      end
+    end
+
+    it 'raises an exception' do
+      expect { chef_run }.to raise_error(Manufacturer::DoesNotExistError)
+    end
   end
 end
